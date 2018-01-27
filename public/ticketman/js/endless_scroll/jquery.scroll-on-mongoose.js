f2c833024410618743e54400173674ca01b59276
jhongertf <jhongertf@gmail.com>
2018-01-27 10:15:13 -0500

    update

--- public/ticketman/js/endless_scroll/jquery.scroll-on-mongoose.js
+++ public/ticketman/js/endless_scroll/jquery.scroll-on-mongoose.js
@@ -50,11 +50,7 @@ MongooseEndlessScroll = (function() {
         var win = $(window);
 
         bottomBoundary = $(document).height() - $(window).height() - _this.options.inflowPixels / 2;
-        console.log($(document).height());
-        console.log(win.height());
-        console.log(win.scrollTop());
-        //if($(document).height() - win.height() == win.scrollTop()){
-        //if($(window).scrollTop() + $(window).height() >= $(document).height()){
+        
         if ($(window).scrollTop() >= bottomBoundary) {
           _this.fetchDown();
           //$(window).scrollTop(bottomBoundary - 20);
