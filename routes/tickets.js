73a9ddf112bca2c2db8f7f696d79ae3cc0ff8d88
jhongertf <jhongertf@gmail.com>
2018-02-12 21:25:53 -0500

    update

--- routes/tickets.js
+++ routes/tickets.js
@@ -7,8 +7,8 @@ var controller;
 controller = require("../controllers/ticket");
 
 router.get('/', ensureLoggedIn, controller.index);
-//router.get('/count.json', ensureLoggedIn, controller.count);
 router.get('/list.json', ensureLoggedIn, controller.list);
+router.get('/count.json', ensureLoggedIn, controller.count);
 router.post('/new', ensureLoggedIn, controller.create);
 router.get('/:id', ensureLoggedIn, controller.show);
 router.post('/:id/abandon', ensureLoggedIn, controller.abandon);
