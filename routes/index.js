49e3850f5fe42dd71d0032d4e07aa29eb5a0139c
jhongertf <jhongertf@gmail.com>
2018-02-13 23:28:15 -0500

    Add index page

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ const env = {
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.redirect("/tickets");
+  res.render("/index");
 });
 
 router.get('/login', passport.authenticate('auth0', {
@@ -28,7 +28,7 @@ router.get('/login', passport.authenticate('auth0', {
 router.get('/logout', function(req, res) {
     req.session.destroy();
     req.logout();
-    res.redirect('/');
+    res.render('/index');
   
 });
 
